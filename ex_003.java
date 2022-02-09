9387d87dec3474573152efe98805fdff3a5b2b23
kjn0414 <kjn0414@hiparking.co.kr>
2022-02-09 16:18:54 +0900

    TEST

--- ex_003.java
+++ ex_003.java
@@ -2,5 +2,7 @@ public class ex_003{
     public static void main(String[] args){
         String str = "It is a TEST";
         System.out.println(str);
+        //TEST
+        System.out.println("TEST");
     }
 }
